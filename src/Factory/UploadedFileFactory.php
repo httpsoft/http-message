8a6c3eaec0378ac104f97bd1c4684418758e2773
devanych <mail@devanych.ru>
2020-08-22 15:24:59 +0300

    Add UploadedFileFactory

---
+++ src/Factory/UploadedFileFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HttpSoft\Message\Factory;
+
+use HttpSoft\Message\UploadedFile;
+use Psr\Http\Message\StreamInterface;
+use Psr\Http\Message\UploadedFileFactoryInterface;
+use Psr\Http\Message\UploadedFileInterface;
+
+use const UPLOAD_ERR_OK;
+
+final class UploadedFileFactory implements UploadedFileFactoryInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function createUploadedFile(
+        StreamInterface $stream,
+        int $size = null,
+        int $error = UPLOAD_ERR_OK,
+        string $clientFilename = null,
+        string $clientMediaType = null
+    ): UploadedFileInterface {
+        $size = (int) ($size === null ? $stream->getSize() : $size);
+        return new UploadedFile($stream, $size, $error, $clientFilename, $clientMediaType);
+    }
+}
---
+++ tests/Factory/UploadedFileFactoryTest.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HttpSoft\Tests\Message\Factory;
+
+use HttpSoft\Message\Factory\UploadedFileFactory;
+use HttpSoft\Message\Stream;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\StreamInterface;
+
+use function file_exists;
+use function sys_get_temp_dir;
+use function tempnam;
+
+use const UPLOAD_ERR_OK;
+
+class UploadedFileFactoryTest extends TestCase
+{
+    /**
+     * @var UploadedFileFactory
+     */
+    private UploadedFileFactory $factory;
+
+    /**
+     * @var string
+     */
+    private string $tmpFile = '';
+
+    public function setUp(): void
+    {
+        $this->factory = new UploadedFileFactory();
+        $this->tmpFile = tempnam(sys_get_temp_dir(), 'httpsoft');
+    }
+
+    public function tearDown(): void
+    {
+        if (file_exists($this->tmpFile)) {
+            unlink($this->tmpFile);
+        }
+    }
+
+    public function testCreateUploadedFile(): void
+    {
+        $uploadedFile = $this->factory->createUploadedFile(
+            $stream = new Stream($this->tmpFile),
+            $size = 1024,
+            UPLOAD_ERR_OK,
+            $clientFilename = 'file.txt',
+            $clientMediaType = 'text/plain'
+        );
+        $this->assertInstanceOf(StreamInterface::class, $uploadedFile->getStream());
+        $this->assertInstanceOf(Stream::class, $uploadedFile->getStream());
+        $this->assertSame($stream, $uploadedFile->getStream());
+        $this->assertSame($size, $uploadedFile->getSize());
+        $this->assertSame(UPLOAD_ERR_OK, $uploadedFile->getError());
+        $this->assertSame($clientFilename, $uploadedFile->getClientFilename());
+        $this->assertSame($clientMediaType, $uploadedFile->getClientMediaType());
+    }
+}
