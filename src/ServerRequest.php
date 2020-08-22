17d92564ce9b42f94882876e9dc41f377aae9434
devanych <mail@devanych.ru>
2020-08-22 12:26:18 +0300

    ServerRequest

---
+++ src/ServerRequest.php
@@ -0,0 +1,244 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HttpSoft\Message;
+
+use InvalidArgumentException;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\StreamInterface;
+use Psr\Http\Message\UploadedFileInterface;
+use Psr\Http\Message\UriInterface;
+
+use function array_key_exists;
+use function gettype;
+use function get_class;
+use function is_array;
+use function is_object;
+use function sprintf;
+
+final class ServerRequest implements ServerRequestInterface
+{
+    use RequestTrait;
+
+    /**
+     * @var array
+     */
+    private array $attributes = [];
+
+    /**
+     * @var array
+     */
+    private array $cookieParams;
+
+    /**
+     * @var array|object|null
+     */
+    private $parsedBody;
+
+    /**
+     * @var array
+     */
+    private array $queryParams;
+
+    /**
+     * @var array
+     */
+    private array $serverParams;
+
+    /**
+     * @var array
+     */
+    private array $uploadedFiles;
+
+    /**
+     * @param array $serverParams
+     * @param array $uploadedFiles
+     * @param array $cookieParams
+     * @param array $queryParams
+     * @param array|object|null $parsedBody
+     * @param string $method
+     * @param UriInterface|string $uri
+     * @param array $headers
+     * @param StreamInterface|string|resource $body
+     * @param string $protocol
+     */
+    public function __construct(
+        array $serverParams = [],
+        array $uploadedFiles = [],
+        array $cookieParams = [],
+        array $queryParams = [],
+        $parsedBody = null,
+        string $method = 'GET',
+        $uri = '',
+        array $headers = [],
+        $body = 'php://input',
+        string $protocol = '1.1'
+    ) {
+        $this->validateUploadedFiles($uploadedFiles);
+        $this->uploadedFiles = $uploadedFiles;
+        $this->serverParams = $serverParams;
+        $this->cookieParams = $cookieParams;
+        $this->queryParams = $queryParams;
+        $this->parsedBody = $parsedBody;
+        $this->init($method, $uri, $headers, $body, $protocol);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getServerParams(): array
+    {
+        return $this->serverParams;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getCookieParams(): array
+    {
+        return $this->cookieParams;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function withCookieParams(array $cookies): self
+    {
+        $new = clone $this;
+        $new->cookieParams = $cookies;
+        return $new;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getQueryParams(): array
+    {
+        return $this->queryParams;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function withQueryParams(array $query): self
+    {
+        $new = clone $this;
+        $new->queryParams = $query;
+        return $new;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getUploadedFiles(): array
+    {
+        return $this->uploadedFiles;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function withUploadedFiles(array $uploadedFiles): self
+    {
+        $this->validateUploadedFiles($uploadedFiles);
+        $new = clone $this;
+        $new->uploadedFiles = $uploadedFiles;
+        return $new;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getParsedBody()
+    {
+        return $this->parsedBody;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function withParsedBody($data): self
+    {
+        if (!is_array($data) && !is_object($data) && $data !== null) {
+            throw new InvalidArgumentException(sprintf(
+                '`%s` is not valid Parsed Body. Must be `null` or a `array` or a `object`.',
+                gettype($data)
+            ));
+        }
+
+        $new = clone $this;
+        $new->parsedBody = $data;
+        return $new;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getAttributes(): array
+    {
+        return $this->attributes;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getAttribute($name, $default = null)
+    {
+        if (array_key_exists($name, $this->attributes)) {
+            return $this->attributes[$name];
+        }
+
+        return $default;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function withAttribute($name, $value): self
+    {
+        if (array_key_exists($name, $this->attributes) && $this->attributes[$name] === $value) {
+            return $this;
+        }
+
+        $new = clone $this;
+        $new->attributes[$name] = $value;
+        return $new;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function withoutAttribute($name): self
+    {
+        if (!array_key_exists($name, $this->attributes)) {
+            return $this;
+        }
+
+        $new = clone $this;
+        unset($new->attributes[$name]);
+        return $new;
+    }
+
+    /**
+     * @param array $uploadedFiles
+     * @throws InvalidArgumentException
+     */
+    private function validateUploadedFiles(array $uploadedFiles): void
+    {
+        foreach ($uploadedFiles as $file) {
+            if (is_array($file)) {
+                $this->validateUploadedFiles($file);
+                continue;
+            }
+
+            if (!$file instanceof UploadedFileInterface) {
+                throw new InvalidArgumentException(sprintf(
+                    'Invalid item in uploaded files structure.'
+                    . '`%s` is not an instance of `Psr\Http\Message\UploadedFileInterface`.',
+                    (is_object($file) ? get_class($file) : gettype($file))
+                ));
+            }
+        }
+    }
+}
---
+++ tests/ServerRequestTest.php
@@ -0,0 +1,296 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HttpSoft\Tests\Message;
+
+use HttpSoft\Message\ServerRequest;
+use HttpSoft\Message\UploadedFile;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\UriInterface;
+use StdClass;
+
+use const UPLOAD_ERR_OK;
+
+class ServerRequestTest extends TestCase
+{
+    private const DEFAULT_METHOD = 'GET';
+
+    /**
+     * @var ServerRequest
+     */
+    private ServerRequest $request;
+
+    public function setUp(): void
+    {
+        $this->request = new ServerRequest();
+    }
+
+    public function testGetDefault(): void
+    {
+        $this->assertSame('/', $this->request->getRequestTarget());
+        $this->assertSame(self::DEFAULT_METHOD, $this->request->getMethod());
+        $this->assertInstanceOf(UriInterface::class, $this->request->getUri());
+        $this->assertSame([], $this->request->getAttributes());
+        $this->assertSame([], $this->request->getServerParams());
+        $this->assertSame([], $this->request->getCookieParams());
+        $this->assertSame([], $this->request->getQueryParams());
+        $this->assertSame([], $this->request->getUploadedFiles());
+        $this->assertNull($this->request->getParsedBody());
+    }
+
+    public function testWithRequestTarget(): void
+    {
+        $request = $this->request->withRequestTarget('*');
+        $this->assertNotSame($this->request, $request);
+        $this->assertSame('*', $request->getRequestTarget());
+    }
+
+    public function testWithRequestTargetHasNotBeenChangedNotClone(): void
+    {
+        $request = $this->request->withRequestTarget(null);
+        $this->assertSame($this->request, $request);
+        $this->assertSame('/', $request->getRequestTarget());
+    }
+
+    /**
+     * @return array
+     */
+    public function invalidRequestTargetProvider(): array
+    {
+        return [['/ *'], ['Request Target'], ["Request\nTarget"], ["Request\tTarget"], ["Request\rTarget"]];
+    }
+
+    /**
+     * @dataProvider invalidRequestTargetProvider
+     * @param mixed $requestTarget
+     */
+    public function testWithRequestTargetThrowExceptionInvalidRequestTarget($requestTarget): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->request->withRequestTarget($requestTarget);
+    }
+
+    public function testWithMethod(): void
+    {
+        $request = $this->request->withMethod($method = 'POST');
+        $this->assertNotSame($this->request, $request);
+        $this->assertSame($method, $request->getMethod());
+
+        $request = $this->request->withMethod($method = 'PoSt');
+        $this->assertNotSame($this->request, $request);
+        $this->assertSame($method, $request->getMethod());
+    }
+
+    public function testWithMethodHasNotBeenChangedNotClone(): void
+    {
+        $request = $this->request->withMethod(self::DEFAULT_METHOD);
+        $this->assertSame($this->request, $request);
+        $this->assertSame(self::DEFAULT_METHOD, $request->getMethod());
+    }
+
+    public function testWithUri(): void
+    {
+        $uri = $this->createMock(UriInterface::class);
+        $request = $this->request->withUri($uri);
+        $this->assertNotSame($this->request, $request);
+        $this->assertSame($uri, $request->getUri());
+    }
+
+    public function testWithUriUpdateHostHeaderFromUri(): void
+    {
+        $request = new ServerRequest([], [], [], [], [], 'GET', 'http://example.com/path/to/action');
+        $this->assertSame(['Host' => ['example.com']], $request->getHeaders());
+        $this->assertSame(['example.com'], $request->getHeader('host'));
+
+        $newUri = $request->getUri()->withHost('example.org');
+
+        $newRequest = $request->withUri($newUri);
+        $this->assertSame(['Host' => ['example.org']], $newRequest->getHeaders());
+        $this->assertSame(['example.org'], $newRequest->getHeader('host'));
+
+        $newRequestWithUriPort = $request->withUri($newUri->withPort(8080));
+        $this->assertSame(['Host' => ['example.org:8080']], $newRequestWithUriPort->getHeaders());
+        $this->assertSame(['example.org:8080'], $newRequestWithUriPort->getHeader('host'));
+
+        $newRequestWithUriStandardPort = $request->withUri($newUri->withPort(80));
+        $this->assertSame(['Host' => ['example.org']], $newRequestWithUriStandardPort->getHeaders());
+        $this->assertSame(['example.org'], $newRequestWithUriStandardPort->getHeader('host'));
+    }
+
+    /**
+     * @return array
+     */
+    public function invalidUriProvider(): array
+    {
+        return $this->getInvalidValues();
+    }
+
+    /**
+     * @dataProvider invalidUriProvider
+     * @param mixed $uri
+     */
+    public function testUriPassingInConstructorThrowExceptionInvalidUri($uri): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new ServerRequest([], [], [], [], [], self::DEFAULT_METHOD, $uri);
+    }
+
+    public function testWithAttributeAndGetAttributes(): void
+    {
+        $request = $this->request->withAttribute('name', 'value');
+        $this->assertNotSame($this->request, $request);
+        $this->assertSame('value', $request->getAttribute('name'));
+        $this->assertSame(['name' => 'value'], $request->getAttributes());
+    }
+
+    public function testWithoutAttributeAndGetAttributes(): void
+    {
+        $firstRequest = $this->request->withAttribute('name', 'value');
+        $this->assertNotSame($this->request, $firstRequest);
+        $this->assertSame('value', $firstRequest->getAttribute('name'));
+        $secondRequest = $firstRequest->withoutAttribute('name');
+        $this->assertNotSame($firstRequest, $secondRequest);
+        $this->assertNull($secondRequest->getAttribute('name'));
+        $this->assertSame([], $secondRequest->getAttributes());
+    }
+
+    public function testGetAttributePassedDefaultValue(): void
+    {
+        $this->assertNull($this->request->getAttribute('name'));
+        $this->assertSame([], $this->request->getAttribute('name', []));
+        $this->assertSame(123, $this->request->getAttribute('name', 123));
+    }
+
+    public function testWithCookieParams(): void
+    {
+        $cookieParams = [
+            'cookie_name' => 'adf8ck8eb43218g8fa5f8259b6425371',
+        ];
+        $request = $this->request->withCookieParams($cookieParams);
+        $this->assertNotSame($this->request, $request);
+        $this->assertSame($cookieParams, $request->getCookieParams());
+    }
+
+    public function testWithQueryParams(): void
+    {
+        $queryParams = [
+            'key1' => 'value1',
+            'key2' => 'value2',
+        ];
+        $request = $this->request->withQueryParams($queryParams);
+        $this->assertNotSame($this->request, $request);
+        $this->assertSame($queryParams, $request->getQueryParams());
+    }
+
+    /**
+     * @return array
+     */
+    public function validParsedBodyProvider(): array
+    {
+        return [
+            'object' => [new StdClass()],
+            'array' => [['key' => 'value']],
+            'empty-array' => [[]],
+        ];
+    }
+
+    /**
+     * @dataProvider validParsedBodyProvider
+     * @param mixed $parsedBody
+     */
+    public function testWithParsedBodyPassedValidParsedBody($parsedBody): void
+    {
+        $request = $this->request->withParsedBody($parsedBody);
+        $this->assertNotSame($this->request, $request);
+        $this->assertSame($parsedBody, $request->getParsedBody());
+    }
+
+    /**
+     * @return array
+     */
+    public function invalidParsedBodyProvider(): array
+    {
+        return [
+            'true' => [true],
+            'false' => [false],
+            'int' => [1],
+            'float' => [1.1],
+            'string' => ['string'],
+        ];
+    }
+
+    /**
+     * @dataProvider invalidParsedBodyProvider
+     * @param mixed $parsedBody
+     */
+    public function testWithParsedBodyThrowExceptionForInvalidParsedBody($parsedBody): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->request->withParsedBody($parsedBody);
+    }
+
+    public function testWithUploadedFiles(): void
+    {
+        $uploadedFiles = [
+            new UploadedFile('file.txt', 1024, UPLOAD_ERR_OK),
+            new UploadedFile('image.png', 67890, UPLOAD_ERR_OK),
+        ];
+
+        $request = $this->request->withUploadedFiles($uploadedFiles);
+        $this->assertNotSame($this->request, $request);
+        $this->assertSame($uploadedFiles, $request->getUploadedFiles());
+    }
+
+    /**
+     * @return array
+     */
+    public function invalidUploadedFilesProvider(): array
+    {
+        return [
+            'array-null' => [[null]],
+            'array-true' => [[true]],
+            'array-false' => [[false]],
+            'array-int' => [[1]],
+            'array-float' => [[1.1]],
+            'array-string' => [['string']],
+            'array-object' => [[new StdClass()]],
+            'array-callable' => [[fn() => null]],
+        ];
+    }
+
+    /**
+     * @dataProvider invalidUploadedFilesProvider
+     * @param mixed $uploadedFiles
+     */
+    public function testWithUploadedFilesThrowExceptionForInvalidUploadedFiles($uploadedFiles): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->request->withUploadedFiles($uploadedFiles);
+    }
+
+    /**
+     * @param array $values
+     * @return array
+     */
+    private function getInvalidValues(array $values = []): array
+    {
+        $common = [
+            'null' => [null],
+            'true' => [true],
+            'false' => [false],
+            'int' => [1],
+            'float' => [1.1],
+            'empty-array' => [[]],
+            'object' => [new StdClass()],
+            'callable' => [fn() => null],
+        ];
+
+        if ($values) {
+            return array_merge($common, $values);
+        }
+
+        return $common;
+    }
+}
