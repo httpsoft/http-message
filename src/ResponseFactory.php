a57136f0dd77b7d5f5f828da7330d766dbe5bef7
devanych <mail@devanych.ru>
2020-08-22 17:03:50 +0300

    Add ResponseFactory

---
+++ src/ResponseFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HttpSoft\Message;
+
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
+
+final class ResponseFactory implements ResponseFactoryInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function createResponse(int $code = 200, string $reasonPhrase = ''): ResponseInterface
+    {
+        return new Response($code, [], 'php://temp', '1.1', $reasonPhrase);
+    }
+}
---
+++ tests/ResponseFactoryTest.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HttpSoft\Tests\Message;
+
+use HttpSoft\Message\Response;
+use HttpSoft\Message\ResponseFactory;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
+
+class ResponseFactoryTest extends TestCase
+{
+    /**
+     * @var ResponseFactory
+     */
+    private ResponseFactory $factory;
+
+    public function setUp(): void
+    {
+        $this->factory = new ResponseFactory();
+    }
+
+    public function testCreateResponse(): void
+    {
+        $response = $this->factory->createResponse();
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+        $this->assertSame(200, $response->getStatusCode());
+        $this->assertSame('OK', $response->getReasonPhrase());
+        $this->assertInstanceOf(StreamInterface::class, $response->getBody());
+        $this->assertSame('php://temp', $response->getBody()->getMetadata('uri'));
+        $this->assertSame([], $response->getHeaders());
+        $this->assertSame('1.1', $response->getProtocolVersion());
+
+        $response = $this->factory->createResponse($code = 404);
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+        $this->assertSame($code, $response->getStatusCode());
+        $this->assertSame('Not Found', $response->getReasonPhrase());
+        $this->assertInstanceOf(StreamInterface::class, $response->getBody());
+        $this->assertSame('php://temp', $response->getBody()->getMetadata('uri'));
+        $this->assertSame([], $response->getHeaders());
+        $this->assertSame('1.1', $response->getProtocolVersion());
+
+        $response = $this->factory->createResponse($code = 404, $customPhrase = 'Custom Phrase');
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+        $this->assertSame($code, $response->getStatusCode());
+        $this->assertSame($customPhrase, $response->getReasonPhrase());
+        $this->assertInstanceOf(StreamInterface::class, $response->getBody());
+        $this->assertSame('php://temp', $response->getBody()->getMetadata('uri'));
+        $this->assertSame([], $response->getHeaders());
+        $this->assertSame('1.1', $response->getProtocolVersion());
+    }
+}
