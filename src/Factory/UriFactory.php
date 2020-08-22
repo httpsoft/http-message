aac5008694b29949c2e332ad18c5b9c135346094
devanych <mail@devanych.ru>
2020-08-22 14:17:59 +0300

    Add UriFactory

---
+++ src/Factory/UriFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HttpSoft\Message\Factory;
+
+use HttpSoft\Message\Uri;
+use Psr\Http\Message\UriFactoryInterface;
+use Psr\Http\Message\UriInterface;
+
+final class UriFactory implements UriFactoryInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function createUri(string $uri = ''): UriInterface
+    {
+        return new Uri($uri);
+    }
+}
---
+++ tests/Factory/UriFactoryTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HttpSoft\Tests\Message\Factory;
+
+use HttpSoft\Message\Factory\UriFactory;
+use HttpSoft\Message\Uri;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\UriInterface;
+
+class UriFactoryTest extends TestCase
+{
+    /**
+     * @var UriFactory
+     */
+    private UriFactory $factory;
+
+    public function setUp(): void
+    {
+        $this->factory = new UriFactory();
+    }
+
+    public function testCreateUri(): void
+    {
+        $uri = $this->factory->createUri($empty = '');
+        $this->assertInstanceOf(Uri::class, $uri);
+        $this->assertInstanceOf(UriInterface::class, $uri);
+        $this->assertSame($empty, (string) $uri);
+
+        $uri = $this->factory->createUri('http://example.com/path/to/tar<>get');
+        $this->assertInstanceOf(Uri::class, $uri);
+        $this->assertInstanceOf(UriInterface::class, $uri);
+        $this->assertSame('http://example.com/path/to/tar%3C%3Eget', (string) $uri);
+    }
+
+    public function testCreateStreamFromFileThrowExceptionForInvalidUri(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->factory->createUri('https:///invalid-uri');
+    }
+}
